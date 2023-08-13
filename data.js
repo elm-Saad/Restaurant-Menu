614f8b92154f4a0b40334536ccfc422a6167cdb5
Saad <128216930+elm-Saad@users.noreply.github.com>
2023-08-13 14:07:25 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,26 @@
+export const menuArray = [
+    {
+        name: "Pizza",
+        imageLink:"pizza.png",
+        alt:"Pizza",
+        ingredients: ["Meat,", "mushrom,", "mozarella"],
+        price: 14,
+        id: 0
+    },
+    {
+        name: "Hamburger",
+        imageLink:"Hamburger.png",
+        alt:"Hamburger",
+        ingredients: ["beef,", "cheese,", "lettuce"],
+        price: 12,
+        id: 1
+    },
+        {
+        name: "shawarma",
+        imageLink:"shawarma.png",
+        alt:"shawarma",
+        ingredients: ["Meat,","Sauce,", "Vegetables"],
+        price: 12,
+        id: 2
+    }
+]
